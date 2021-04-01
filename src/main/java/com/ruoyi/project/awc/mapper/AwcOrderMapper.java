6415de37a69a8bdd0fcdb17795ff2f076ce402f4
han <hzy717@qq.com>
2021-04-01 21:27:37 +0800

    删除自带文件

--- src/main/java/com/ruoyi/project/awc/mapper/AwcOrderMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ruoyi.project.awc.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ruoyi.project.awc.entity.AwcOrder;
-
-public interface AwcOrderMapper extends BaseMapper<AwcOrder> {
-
-}
